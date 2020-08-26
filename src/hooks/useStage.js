78bcd5e637bbfdb54c82683d2d730089e6361d63
codershona <falgunimst95@gmail.com>
2020-08-26 16:38:59 +0200

    Clear rows logic added

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,16 +4,41 @@ import { createStage } from '../gameHelpers';
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
 
+  const [rowsCleared, setRowsCleared] = useState(0);
+
+
+
   useEffect(() => {
+
+    setRowsCleared(0);
+
+    const sweepRows = newStage => 
+      newStage.reduce((ack, row) => {
+
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+
+          setRowsCleared(prev => prev + 1);
+
+          ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+
+          return ack;
+
+        }
+
+        ack.push(row);
+
+        return ack;
+
+      }, [])
 
   	const updateStage = prevStage => {
 
   		const newStage = prevStage.map(row => 
   			row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
 
-
-
   			);
+
+
 
   		player.tetromino.forEach((row, y) => {
 
@@ -35,6 +60,9 @@ export const useStage = (player, resetPlayer) => {
 
         resetPlayer();
 
+        return sweepRows(newStage);
+
+
       }
 
   		return newStage;
@@ -51,7 +79,7 @@ export const useStage = (player, resetPlayer) => {
 
 
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 
 
 };
